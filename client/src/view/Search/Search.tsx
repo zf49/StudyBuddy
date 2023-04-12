a056a65e647ff6b40691827daa626314ad1ccea4
zf49 <zwan686@aucklanduni.ac.nz>
2023-04-12 14:59:35 +1200

    search result front-end

--- client/src/view/Search/Search.tsx
+++ client/src/view/Search/Search.tsx
@@ -1,66 +1,103 @@
 import React from 'react'
 import { useState } from 'react';
-import { TextField, Button, Grid, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { TextField, Button, Grid, Typography, List } from '@mui/material';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+
 import axios from 'axios';
 
 const theme = createTheme();
 
 export default function Search() {
 
-// define key word
-const [searchTerm, setSearchTerm] = useState<string>('');
+    // define key word
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [searchRes, setSearchRes] = useState<string[]>();
 
-// handel key word change 
-const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setSearchTerm(event.target.value);
-};
-
-const handleSearchClick = async() => {
-  // TODO: logic of search friend
-  console.log(`Searching for friends with term: ${searchTerm}`);
-
-  await axios.post('http://localhost:8080/users/:keyword').then((res)=>{
-    console.log(res.data)
-  })
+    // handel key word change 
+    const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
 
 
 
-};
+    const handleSearchClick = async () => {
+        // TODO: logic of search friend
+        console.log(`Searching for friends with term: ${searchTerm}`);
 
-return (
-    <ThemeProvider theme={theme}>
-    <Grid container justifyContent="center" alignItems="center" spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h4" align="center">
-          Search Friends
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Enter search term"
-          value={searchTerm}
-          onChange={handleSearchTermChange}
-          InputProps={{
-            endAdornment: (
-              <Button variant="contained" onClick={handleSearchClick}>
-                Search
-              </Button>
-            )
-          }}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant="h4" align="center">
-          Search Friends
-        </Typography>
+        await axios.post(`http://localhost:8080/users/${searchTerm}`).then((res) => {
+            // console.log(res.data)
+            setSearchRes(res.data)
+        })
 
-        
-        
-      </Grid>
-    </Grid>
-  </ThemeProvider>
-);
+
+
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container justifyContent="center" alignItems="center" spacing={2}>
+                <Grid item xs={12}>
+                    <Typography variant="h4" align="center">
+                        Search Friends
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        label="Enter search term"
+                        value={searchTerm}
+                        onChange={handleSearchTermChange}
+                        InputProps={{
+                            endAdornment: (
+                                <Button variant="contained" onClick={handleSearchClick}>
+                                    Search
+                                </Button>
+                            )
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h4" align="center">
+                        Results
+                    </Typography>
+                    <Typography variant="h4" align="center">
+
+                        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Remy Sharp" src="https://i.pravatar.cc/150?img=1" />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Brunch this weekend?"
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                sx={{ display: 'inline' }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
+                                            >
+                                                Ali Connors
+                    </Typography>
+                                            {" — I'll be in your neighborhood doing errands this…"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                            <Divider variant="inset" component="li" />
+                        </List>
+                    </Typography>
+
+
+
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
 }
