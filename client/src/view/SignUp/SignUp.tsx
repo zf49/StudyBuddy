312325b5ac244df63290a5a3619a1b973433ab36
Cody <wpan088@aucklanduni.ac.nz>
2023-04-27 11:15:54 +1200

    Fix signup page UI

--- client/src/view/SignUp/SignUp.tsx
+++ client/src/view/SignUp/SignUp.tsx
@@ -198,6 +198,7 @@ export default function SignUp(props: ISignUpProps) {
                     <InputLabel>Gender</InputLabel>
                     <Select
                         id="gender"
+                        label="Gender"
                         value={gender}
                         onChange={handleGender}
                     >
@@ -223,6 +224,7 @@ export default function SignUp(props: ISignUpProps) {
                     <InputLabel>Faculty</InputLabel>
                     <Select
                         id="faculty"
+                        label="Faculty"
                         value={faculty}
                         onChange={handleFaculty}
                     >
@@ -242,6 +244,7 @@ export default function SignUp(props: ISignUpProps) {
                     <InputLabel>Major</InputLabel>
                     <Select
                         id="major"
+                        label="Major"
                         value={major}
                         onChange={handleMajor}
                     >
