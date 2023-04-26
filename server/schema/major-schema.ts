351098c5d6b0802534d3f43ae765bf7cdc0b7b52
PathToLife <12622625+PathToLife@users.noreply.github.com>
2023-04-26 15:40:05 +1200

    minor refactor schema interface name

--- server/schema/major-schema.ts
+++ server/schema/major-schema.ts
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-export interface IMajor {
-    faculty: string,
-    major: string
+export interface IMajorOption {
+  faculty: string;
+  major: string;
 }
 
-const majorSchema = new Schema<IMajor>({
-    faculty: {type: String, required: true},
-    major: {type: String, required: true},
-})
+const majorSchema = new Schema<IMajorOption>({
+  faculty: { type: String, required: true },
+  major: { type: String, required: true },
+});
 
-const Major = mongoose.model("Major", majorSchema)
+const Major = mongoose.model("Major", majorSchema);
 
-export {Major}
+export { Major };
