35c15e19edfe0cd223753665620b22b5f069895d
zf49 <zwan686@aucklanduni.ac.nz>
2023-05-08 14:23:43 +1200

    gmail test

--- server/controller/emailController.ts
+++ server/controller/emailController.ts
@@ -9,21 +9,32 @@ interface MailOptions {
 
 export const emailSend = async (): Promise<boolean> => {
 
+    // const transporter = nodemailer.createTransport({
+    //     service: 'hotmail',
+    //     auth: {
+    //         user: 'wangzhifang97@live.com',
+    //         pass: 'wzf!@#000'
+    //     },
+    //     host: "smtp.live.com",
+    //     port: 587,
+    //     secureConnection: false,
+    //     tls: {
+    //         ciphers:'SSLv3'
+    //     }
+    // });
     const transporter = nodemailer.createTransport({
-        service: 'hotmail',
+        service: 'gmail',
         auth: {
-            user: 'wangzhifang97@live.com',
-            pass: 'wzf!@#000'
+            user: 'wangzhifang000@gmail.com',
+            pass: 'wzf!@#00'
         },
-        host: "smtp.live.com",
-        port: 587,
-        secureConnection: false,
-        tls: {
-            ciphers:'SSLv3'
-        }
-    });
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true
+    })
+    
     const mailOptions: MailOptions = {
-        from: 'wangzhifang97@live.com',
+        from: 'wangzhifang000@gmail.com',
         to: 'wangzhifang97@live.com',
         subject: 'Someone follow you!',
         text: '666666'
