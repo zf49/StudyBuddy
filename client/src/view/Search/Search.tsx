ede23ee6caceae4bb54fe51917d03c03155dae05
zf49 <zwan686@aucklanduni.ac.nz>
2023-05-10 01:19:57 +1200

    fix params cannot carry to friendsdetail

--- client/src/view/Search/Search.tsx
+++ client/src/view/Search/Search.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react'
-import { useState,useEffect } from 'react';
-import { TextField, Button, Grid, Typography, List, Menu, MenuItem, Chip } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { TextField, Button, Grid, Typography, List, Menu, MenuItem, Chip, IconButton } from '@mui/material';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
@@ -21,11 +21,11 @@ import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, Tab
 import { styled } from '@mui/material/styles';
 import Recommendation from './Recommendation';
 import TestRoll from './testroll';
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontWeight: 'bold',
     borderBottom: `1px solid ${theme.palette.divider}`,
-  }));
+}));
 
 
 
@@ -35,7 +35,7 @@ export default function Search() {
     const users = useSelector((state: RootState) => state.storeUser.userList);
     // define key word
     const [searchTerm, setSearchTerm] = useState<string>('');
-    
+
     const [searchRes, setSearchRes] = useState<IUserDetail[]>(users);
 
     const [flag, setFlag] = useState(true)
@@ -47,7 +47,7 @@ export default function Search() {
         return () => {
             controller.abort()
         }
-    },[])
+    }, [])
 
     // handel key word change 
     const handleSearchTermChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,142 +55,158 @@ export default function Search() {
     };
 
     // main logic of search user
-    const handleSearchClick =  () => {
+    const handleSearchClick = () => {
         // TODO: logic of search friend
         setFlag(false)
-             axios.post(`http://localhost:8080/users/${searchTerm}`,{signal: controller.signal}).then((res) => {
-                 if(res.data.length!==0){
-                    setSearchRes(res.data)
-                    dispatch(storeUser(res.data))                 
-                 }
-            })
+        axios.post(`http://localhost:8080/users/${searchTerm}`, { signal: controller.signal }).then((res) => {
+            if (res.data.length !== 0) {
+                setSearchRes(res.data)
+                dispatch(storeUser(res.data))
+            }
+        })
     };
 
     // when user press Enter, show search result 
-    const handleKeyDown =  (e:React.KeyboardEvent<HTMLInputElement>) => {
-        if(e.key==='Enter'){
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
             handleSearchClick()
         }
     };
 
     //redirect to /frienddetail/
-    const handleFriendDetail = (id:string)=>{
+    const handleFriendDetail = (id: string) => {
         console.log(id)
-        navigate("/frienddetail", { state: { "id": id }})
+        navigate("/frienddetail", { state: { "id": id } })
         // navigate(`/frienddetail?id=${id}`)
 
     }
 
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-          setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const handleSelectedCourses = (selectedCourses:string[]) => {
-    console.log(selectedCourses);
+    const handleSelectedCourses = (selectedCourses: string[]) => {
+        console.log(selectedCourses);
 
-    const arr:IUserDetail[] = []
+        const arr: IUserDetail[] = []
 
-    searchRes.map((item)=>{
-        selectedCourses.forEach(element => {
-            item.courses.map((course)=>{
-                if(element === course.CourseNName){
-                    arr.push(item)
-                }
-            })
-        });
-    })
-    const newArr = Array.from(new Set(arr));
-    console.log(newArr)
-    setSearchRes(newArr)
-  };
- 
+        searchRes.map((item) => {
+            selectedCourses.forEach(element => {
+                item.courses.map((course) => {
+                    if (element === course.CourseNName) {
+                        arr.push(item)
+                    }
+                })
+            });
+        })
+        const newArr = Array.from(new Set(arr));
+        console.log(newArr)
+        setSearchRes(newArr)
+    };
+
     return (
         <div style={{ width: "100%", textAlign: "center", margin: "0 auto" }}>
-          <div>
-            <h1>Search Buddies</h1>   
-            <Paper sx={{ p: 2}}>
-                <Grid container justifyContent="center" alignItems="center" spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                        onKeyDown={handleKeyDown}
-                        fullWidth
-                        variant="outlined"
-                        label="Search Buddies"
-                        value={searchTerm}
-                        onChange={handleSearchTermChange}
-                        InputProps={{
-                            endAdornment: (
-                                <>
-                                <Button onClick={handleClickOpen}>
-                                <FilterAltIcon />
+            <div>
+                <h1>Search Buddies</h1>
+                <Paper sx={{ p: 2 }}>
+                    <Grid container justifyContent="center" alignItems="center" spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                onKeyDown={handleKeyDown}
+                                fullWidth
+                                variant="outlined"
+                                label="Search Buddies"
+                                value={searchTerm}
+                                onChange={handleSearchTermChange}
+                                InputProps={{
+                                    endAdornment: (
+                                        <>
+                                            <Button onClick={handleClickOpen}>
+                                                <FilterAltIcon />
+                                            </Button>
+                                            <Button variant="contained"
+                                                onClick={handleSearchClick}>
+                                                Search
                                 </Button>
-                                <Button variant="contained" 
-                                onClick={handleSearchClick}>
-                                    Search 
-                                </Button>
-                                </>
-                            )
-                        }}
-                        />
-                </Grid>
-                <ResultFilter open={open} 
+                                        </>
+                                    )
+                                }}
+                            />
+                        </Grid>
+                        <ResultFilter open={open}
                             handleClickOpen={handleClickOpen}
                             handleClose={handleClose}
                             users={searchRes}
                             onSelectedCoursesChange={handleSelectedCourses}
-                            // onSubmit={handleDataChange}
-                        /> 
-                </Grid>
-                {flag?<Recommendation/>:
-                <Box>
-                <Paper elevation={1}>
-                <TableContainer component={Paper} >
-                    <Table>
-                    <TableHead>
-                        <TableRow>
-                        <StyledTableCell></StyledTableCell>
-                        <StyledTableCell>Name</StyledTableCell>
-                        <StyledTableCell>UniID</StyledTableCell>
-                        <StyledTableCell>Email</StyledTableCell>
-                        <StyledTableCell>Faculty</StyledTableCell>
-                        <StyledTableCell>Major</StyledTableCell>
-                        <StyledTableCell>Courses</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {searchRes.map((item) => (
-                        <TableRow key={item._id} onClick={() => handleFriendDetail(item._id)}>
-                            <TableCell><Avatar alt="Remy Sharp" src={item.userAvatar} /></TableCell>
-                            <TableCell>{item.name}</TableCell>
-                            <TableCell>{item.uniID}</TableCell>
-                            <TableCell>{item.email}</TableCell>
-                            <TableCell>{item.faculty}</TableCell>
-                            <TableCell>{item.major}</TableCell>
-                            <TableCell>{item.courses.map((item)=>{
-                                            return <Chip style={{
-                                                marginBottom:'0.2em'
-                                            }} key={item.course_code} label={item.CourseNName}></Chip>
-                                        })}</TableCell>
-                        </TableRow>
-                        ))}
-                    </TableBody>
-                    </Table>
-                </TableContainer>
+                        />
+                    </Grid>
+                    {flag ? <Recommendation /> :
+
+                        <Box display="flex" flexDirection="column" alignItems="center">
+                            {searchRes.map((item) => (
+                                <Box
+                                    key={item._id}
+                                    sx={{
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        marginBottom: "1em",
+                                        padding: "1em",
+                                        boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+                                        borderRadius: "5px",
+                                        width: "100%",
+                                    }}
+                                    onClick={() => handleFriendDetail(item._id)}
+                                >
+                                    <Avatar alt="Remy Sharp" src={item.userAvatar} sx={{ marginRight: "1em" }} />
+                                    <Grid container spacing={1}>
+                                        <Grid item xs={12}>
+                                            <Typography variant="h6">{item.name}</Typography>
+                                        </Grid>
+                                        <Grid item xs={12} md={6}>
+                                            <Typography>{item.uniID}</Typography>
+                                        </Grid>
+                                        <Grid item xs={12} md={6}>
+                                            <Typography>{item.email}</Typography>
+                                        </Grid>
+                                        <Grid item xs={12} md={6}>
+                                            <Typography>{item.faculty}</Typography>
+                                        </Grid>
+                                        <Grid item xs={12} md={6}>
+                                            <Typography>{item.major}</Typography>
+                                        </Grid>
+                                        <Grid item component="div" xs={12}>
+                                            <Box display="flex" justifyContent="center" alignItems="center" flexWrap="wrap">
+                                                {item.courses.map((course) => (
+                                                    <Chip
+                                                        key={course.course_code}
+                                                        label={course.CourseNName}
+                                                        sx={{ marginRight: "0.5em", marginBottom: "0.5em" }}
+                                                    />
+                                                ))}
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+                                    <IconButton onClick={() => handleFriendDetail(item._id)}>
+                                        <ArrowForwardIcon />
+                                    </IconButton>
+                                </Box>
+                            ))}
+                        </Box>
+
+                    }
                 </Paper>
-             </Box>}  
-             </Paper>                
-         </div>
-        </div>
+            </div>
+        </div>     
     );
 }
 
-function useHistory() {
-    throw new Error('Function not implemented.');
-}
+
 
